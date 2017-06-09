9a37c9c9e8de7296b6ca4ccc0eb04b6f36ba4a32
clarkdenunzio <clark@clarkdenunzio.com>
2017-06-08 17:27:49 -1000

    person 14 name appears

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ console.log("sanity check");
 
 var oReq = new XMLHttpRequest();
 var oReq1 = new XMLHttpRequest();
+var oReq2 = new XMLHttpRequest();
+var oReq3 = new XMLHttpRequest();
 
 (function(){
 
@@ -17,6 +19,17 @@ var oReq1 = new XMLHttpRequest();
     person4HomeWorld.innerHTML = response.name;
   };
 
+  var reqListener2 = function(){
+    var response = JSON.parse(this.responseText);
+    var person14Name = document.getElementById('person14Name');
+    person14Name.innerHTML = response.name;
+  };
+
+  var reqListener3 = function(){
+    var respone = JSON.parse(this.responseText);
+    var person14Species = document.getElementById(person14Species);
+  };
+
   oReq.addEventListener('load', reqListener);
   oReq.open('GET', 'http://www.swapi.co/api/people/4');
   oReq.send();
@@ -24,6 +37,10 @@ var oReq1 = new XMLHttpRequest();
   oReq1.addEventListener('load', reqListener1);
   oReq1.open('GET', 'http://www.swapi.co/api/planets/1/');
   oReq1.send();
+
+  oReq2.addEventListener('load', reqListener2);
+  oReq2.open('GET', 'http://swapi.co/api/people/14/');
+  oReq2.send();
 
 
 })();
